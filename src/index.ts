281abcfe92297a5c1579534909242211cce1f38d
DEPSTRCZ <jiri.edelmann@gmail.com>
2022-08-30 21:39:07 +0200

    index.ts small fixes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 // Imports
-const config = require("./config/config.json");
-const fs = require("node:fs");
-const TEMP = require("./chachesystem/temp.json");
-const { Client, GatewayIntentBits } = require("discord.js");
+import config from "./config/config.json";
+import fs from "node:fs";
+import TEMP from "./chachesystem/temp.json";
+import { Client, GatewayIntentBits } from "discord.js";
 //intents - client
 const INTENTS = [
     GatewayIntentBits.DirectMessages,
@@ -24,19 +24,19 @@ const SlashCommandsHandler = require("./handlers/slashcommands");
 EventsHandler(client);
 SlashCommandsHandler(client);
 //Auto deploy cmds
-fs.readFile("./chachesystem/temp.json", (err, data) => {
+fs.readFile("./chachesystem/temp.json", (err:any, data:any) => {
     if (!err || data) {
         const file = JSON.parse(data.toString());
-        const length = fs.readdirSync("./commands/", "./commands/fun").length;
+        const length = fs.readdirSync("./commands/").length;
         file.TEMP.editDeploy = length;
-        fs.writeFile("./chachesystem/temp.json", JSON.stringify(file), (err) => {
+        fs.writeFile("./chachesystem/temp.json", JSON.stringify(file), (err:any) => {
             if (err) console.log(err);
         });
     }
 });
 const length = fs.readdirSync("./dist/commands/").length;
 if (TEMP.TEMP.editDeploy !== length) {
-    commandFile = require("./handlers/autodeploy");
+    let commandFile = require("./handlers/autodeploy");
     commandFile.run(client);
 } else {
     console.log("[", "\x1b[43m", "Commands", "\x1b[0m", "]", "\x1b[0m", " No edit to commands were made... Skipping");
