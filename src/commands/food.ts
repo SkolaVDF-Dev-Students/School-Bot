c6706743cf8b6cafcad419872dba1beafea48628
Adam Lipert <a.lipert@protonmail.com>
2022-09-01 21:02:49 +0200

    Add new stuff

--- src/commands/food.ts
+++ src/commands/food.ts
@@ -1,8 +1,22 @@
 import { Embed, Client, EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import axios from "axios";
+
+async function getStravaData(format: string): Promise<string> {
+    let data = await axios.get("https://www.strava.cz/strava5/Jidelnicky?zarizeni=6218");
+    return data.data;
+}
 
 module.exports = {
-    data: new SlashCommandBuilder().setName("food").setDescription("Vrátí jídelníček pro vybrané "),
+    data: new SlashCommandBuilder()
+        .setName("food")
+        .setDescription("Výběr možností")
+        .addSubcommand((subcommand) => subcommand.setName("today").setDescription("Pošle dnešní jídelníček."))
+        .addSubcommand((subcommand) => subcommand.setName("tomorrow").setDescription("Pošle zítřejší jídelníček."))
+        .addSubcommand((subcommand) => subcommand.setName("month").setDescription("Pošle jídelníček na celý měsíc."))
+        .addSubcommand((subcommand) => subcommand.setName("permanent").setDescription("Pošle zprávu s dnešním jídelníčkem který se každých 24h aktualizuje.")),
     async execute(interaction: any) {
-        return interaction.reply("asda");
+        let asdass: string = await getStravaData(interaction.options._subcommand);
+
+        return interaction.reply({ content: asdass });
     },
 };
