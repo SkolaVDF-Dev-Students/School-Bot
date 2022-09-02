b12270b8b2226fb57b5246689a02ff334ef02653
Adam Lipert <a.lipert@protonmail.com>
2022-09-02 11:31:32 +0200

    today message without embed

--- src/commands/food.ts
+++ src/commands/food.ts
@@ -4,9 +4,50 @@ import { toJson as xml2json } from "xml2json";
 
 async function getStravaData(format: string): Promise<string> {
     let data = await axios.get("https://www.strava.cz/strava5/Jidelnicky/XML?zarizeni=6218");
+
+    if (data.status != 200) return data.statusText;
+
     let parsedToJson: any = xml2json(data.data);
     let parsed: any = JSON.parse(parsedToJson);
-    return parsed.jidelnicky.den[0].jidlo[0].nazev;
+
+    switch (format) {
+        case "today":
+            let today = new Date();
+            const yyyy: number = today.getFullYear();
+            let mm: number = today.getMonth() + 1; // Months start at 0!
+            let dd: number = today.getDate();
+
+            let dd2: string = "";
+            let mm2: string = "";
+
+            if (dd < 10) {
+                dd2 = dd.toString();
+                dd2 = `0${dd2}`;
+            }
+
+            if (mm < 10) {
+                mm2 = mm.toString();
+                mm2 = `0${mm2}`;
+            }
+
+            const formattedToday = `${dd2}-${mm2}-${yyyy}`;
+
+            let days = parsed.jidelnicky.den;
+
+            let foods: string = "";
+
+            days.forEach((day: any) => {
+                if (day.datum == formattedToday) {
+                    day.jidlo.forEach((food: any) => {
+                        foods += `${food.nazev} - ${food.druh} - ${food.alergeny}\n`;
+                    });
+                }
+            });
+            return foods;
+
+        default:
+            return "Something happened whoops";
+    }
 }
 
 module.exports = {
@@ -15,7 +56,7 @@ module.exports = {
         .setDescription("Výběr možností")
         .addSubcommand((subcommand) => subcommand.setName("today").setDescription("Pošle dnešní jídelníček."))
         .addSubcommand((subcommand) => subcommand.setName("tomorrow").setDescription("Pošle zítřejší jídelníček."))
-        .addSubcommand((subcommand) => subcommand.setName("month").setDescription("Pošle jídelníček na celý měsíc."))
+        .addSubcommand((subcommand) => subcommand.setName("date").setDescription("Pošle jídelníček na napsané datum."))
         .addSubcommand((subcommand) => subcommand.setName("permanent").setDescription("Pošle zprávu s dnešním jídelníčkem který se každých 24h aktualizuje.")),
     async execute(interaction: any) {
         let asdass: string = await getStravaData(interaction.options._subcommand);
