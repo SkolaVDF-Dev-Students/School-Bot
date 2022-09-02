ebe50381d46a1b69b44d2424a30e1c2865a5c63b
DEPSTRCZ <jiri.edelmann@gmail.com>
2022-09-02 22:48:55 +0200

    Handling for random word done

--- src/commands/random.ts
+++ src/commands/random.ts
@@ -1,6 +1,10 @@
 
 import { Embed, Client, EmbedBuilder, SlashCommandBuilder, } from "discord.js";
 import axios from "axios";
+import randomwords from "random-words"
+import * as deepl from 'deepl-node';
+const authKey = "6469a68d-1cf7-b681-ed7f-8fa091941857:fx"; // Replace with your key
+const translator = new deepl.Translator(authKey);
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('nahodné')
@@ -9,26 +13,37 @@ module.exports = {
             subcommand
                 .setName('slovo')
                 .setDescription('Pošle náhodné slovo')
-                .addIntegerOption(option => option.setName('počet').setDescription('Vybere kolik náhodných slov má poslat. Max 30')))
+                .addIntegerOption(option =>
+                    option.setName("počet")
+                        .setDescription("Vybere maximální počet vygenerovaných slov. Max 20")
+                        .setRequired(true))
+                .addStringOption(option =>
+                    option.setName('jazyk')
+                        .setDescription('Vybere v jakém jazyce májí být náhodné slova')
+                        .setRequired(true)
+                        .addChoices(
+                            { name: 'Česky', value: 'cs' },
+                            {name: 'Anglicky', value: 'en' },)))        
         .addSubcommand(subcommand =>
             subcommand
                 .setName('číslo')
                 .setDescription('Pošle náhodnné číslo')),
 	async execute(interaction:any) {
-        let slovo = interaction.options.getSubcommand("slovo");
-        let číslo = interaction.options.getSubcommand("číslo");
-        if (slovo) {
-            if (interaction.options.getInteger("počet") > 30) return interaction.reply({content: "EMBED: Amount to big",embeds: [], ephemeral: true});
+        if (interaction.options.getSubcommand("slovo")) {
+            if (interaction.options.getInteger("počet") > 20) return interaction.reply({content: "Vybral jsi až moc velké číslo"})
             await interaction.deferReply();
-            let randomresponse = [];
+            let randomresponse = "";
             for (let i = 0; i < interaction.options.getInteger("počet"); i++) {
-                const fetch = await axios.get("https://api.api-ninjas.com/v1/randomword",{
-                    headers: {"X-Api-Key":"uEvz3m9YkP9MnP9EaMGJ2A==mzX2yJ1fhEtu1jYJ"} //move to conf
-                })
-                await randomresponse.push(fetch.data.word)
+                if (interaction.options.getString('jazyk') == "cs") {
+                    const result:any = await translator.translateText(randomwords(), null, 'cs');
+                    randomresponse += `${result.text}\n`;
+                } else {
+                    randomresponse += `${randomwords()}\n`;
+                }
             };
-            interaction.editReply({content: "cigus"});
-        } else if (číslo) {
+            if (interaction.options.getString("jazyk") == "cs") return interaction.editReply({content: "CS: Může být nepřesný \n\n" + randomresponse})
+            if (interaction.options.getString("jazyk") == "en") return interaction.editReply({content: "En: Přesný \n\n" + randomresponse})
+        } else if (interaction.options.getSubcommand("číslo")) {
 
         }
 	},
