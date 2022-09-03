97d37e8391b4d6ff78153581bb96141959ca3764
DEPSTRCZ <jiri.edelmann@gmail.com>
2022-09-03 20:02:41 +0200

    Update, begining of (číslo)

--- src/commands/random.ts
+++ src/commands/random.ts
@@ -27,10 +27,29 @@ module.exports = {
         .addSubcommand(subcommand =>
             subcommand
                 .setName('číslo')
-                .setDescription('Pošle náhodnné číslo')),
+                .setDescription('Pošle náhodnné číslo')
+                .addIntegerOption(option =>
+                    option.setName("počet")
+                        .setDescription("Vybere maximální počet vygenerovaných čísel. Max 50")
+                        .setRequired(true))
+                .addStringOption(option => 
+                    option.setName('od')
+                    .setDescription('Umožňuje ti specifikovat od kterého čísla se má/mají náhodné číslo/a generovat'))
+                .addStringOption(option => 
+                    option.setName('do')
+                        .setDescription('Umožňuje ti specifikovat do kterého čísla se má/mají náhodné číslo/a generovat'))
+                .addStringOption(option =>
+                    option.setName('délka')
+                        .setDescription('Vybere v jakém jazyce májí být náhodné slova')
+                        .addChoices(
+                            { name: '9 - Max', value: '1' },
+                            { name: '99 - Max', value: '2' },
+                            { name: '999 - Max', value: '3' },
+                            { name: '9999 - Max', value: '4' },
+                        ))),
 	async execute(interaction:any) {
         if (interaction.options.getSubcommand("slovo")) {
-            if (interaction.options.getInteger("počet") > 20) return interaction.reply({content: "Vybral jsi až moc velké číslo"})
+            if (interaction.options.getInteger("počet") > 20) return interaction.reply({content: "EMBED: Vybral jsi až moc velké číslo Max 20"})
             await interaction.deferReply();
             let randomresponse = "";
             for (let i = 0; i < interaction.options.getInteger("počet"); i++) {
@@ -44,7 +63,9 @@ module.exports = {
             if (interaction.options.getString("jazyk") == "cs") return interaction.editReply({content: "CS: Může být nepřesný \n\n" + randomresponse})
             if (interaction.options.getString("jazyk") == "en") return interaction.editReply({content: "En: Přesný \n\n" + randomresponse})
         } else if (interaction.options.getSubcommand("číslo")) {
-
+            if (interaction.options.getInteger("počet") > 20) return interaction.reply({content: "EMBED: Vybral jsi až moc velké číslo Max 50"})
+            await interaction.deferReply();
+            let randomresponse = "";
         }
 	},
 };
