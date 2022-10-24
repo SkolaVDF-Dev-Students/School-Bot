0ceb66741c12318cad2583be0d01f9e303def654
DEPSTRCZ <jiri.edelmann@gmail.com>
2022-10-24 20:24:31 +0200

    Handler
    Co-authored-by: LosNormalis <LosNormalis@users.noreply.github.com>

--- src/handlers/slashcommands.ts
+++ src/handlers/slashcommands.ts
@@ -1,49 +1,31 @@
 import { Client, Collection, EmbedBuilder } from "discord.js";
 import path from "node:path";
 import fs, { Dir, read, readdir } from "node:fs";
-
+import { nest_limit } from "../configs/bot/slashcommands.json"
+let commandFiles:any = [];
+async function LoopDir(dir:string, level:number) {
+    if(level > nest_limit && nest_limit) throw new Error("MOC HLUBUKU XD")
+    const elements = fs.readdirSync(dir);
+    
+    for(const element of elements) {
+        if(element.endsWith(".ts")) commandFiles.push(dir+element);
+        else await LoopDir(dir+element+"\\", level++);
+    }
+}
+//made with BIG assistance of da Milk MaN
 export default async function SlashCommandsHandler(client: any) {
     client.commands = new Collection();
     const commandsPath = path.join(__dirname, '../commands/');
     //const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
-    let commandFiles:any = [];
-    let Dirs:any = [];
-    fs.readdirSync(commandsPath).forEach(element => {
-        if(element.endsWith(".ts")) commandFiles.push(commandsPath+element)
-        if(!element.endsWith(".ts")) Dirs.push(commandsPath+element)
-    })
-    while (Dirs.length > 0) {
-        Dirs.forEach(async (file: string) => {
-            let readdir:any = fs.readdirSync(file);
-                if(readdir.length === 0) {
-                    Dirs = Dirs.filter((element:any) => element !== file)
-                    return
-                }
-                console.log(file)
-                console.log(Dirs)
-                readdir.forEach((element:any) => {
-                    if(element.endsWith(".ts")) commandFiles.push(path.join(__dirname,element))
-                    if(!element.endsWith(".ts")) Dirs.push(path.join(__dirname,element))
-                    Dirs = Dirs.filter((element:any) => element !== file)
-                })
-        });
+    await LoopDir(commandsPath, 0);
+    console.log(commandFiles);
+
         
-    }
-    //console.log(commandFiles)
-    //console.log(__dirname)
-    //do it as a object and save category
-    //console.log(commandFiles)
-    //console.log(Dirs)
-    /*Dirs.forEach(async (file: any) => {
-        const t = await import(file)
-        console.log(t.name)
-    });*/
-        
-    /*for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = await import(filePath);
+    for (const file of commandFiles) {
+        const command = await import(file);
         console.log("[","\x1b[43m","C","\x1b[0m","]","\x1b[4m", file, "\x1b[0m" + " Loaded!")
-        client.commands.set(command.data.name, command);
+        console.log(command.data);
+        //client.commands.set(command.data.name, command);
     }
     client.on('interactionCreate', async (interaction:any) => {
         if (!interaction.isChatInputCommand()) return;
@@ -60,5 +42,5 @@ export default async function SlashCommandsHandler(client: any) {
             .setDescription("There was an error while executing this command!")
             await interaction.reply({ embeds: [ERROR], ephemeral: true });
         }
-    });*/
+    });
 }
