1f7c92643722b8da3c696c24f4cfaf5ebc2b4230
DEPSTRCZ <jiri.edelmann@gmail.com>
2022-11-09 20:59:30 +0100

    New handler

--- src/handlers/events.ts
+++ src/handlers/events.ts
@@ -1,13 +1,28 @@
 import path from "node:path";
-import fs from "node:fs";
-
+import fs, { Dir, read, readdir } from "node:fs";
+import { nest_limit } from "../configs/bot/slashcommands.json"
+let eventsFiles:any = [];
+async function LoopDir(dir:string, level:number) {
+    if(level > nest_limit && nest_limit) {
+        console.log("[","\x1b[41m","Error","\x1b[0m","]")
+        throw new Error("Folder nest limit reached");
+    }
+    const elements = fs.readdirSync(dir);
+    
+    for(const element of elements) {
+        if(element.endsWith(".ts")) eventsFiles.push(dir+element);
+        else await LoopDir(dir+element+"\\", level++);
+    }
+}
 export default async function EventsHandler(client:any) {
     const eventsPath = path.join(__dirname, '../events/');
-    const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.ts'));
-    for (const file of eventFiles) {
-        const filePath = path.join(eventsPath, file);
-        const event = await import(filePath);
-        console.log("[","\x1b[42m","E","\x1b[0m","]","\x1b[4m", file, "\x1b[0m" + " Loaded!")
+    //const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.ts'));
+    await LoopDir(eventsPath, 0);
+
+    for (const file of eventsFiles) {
+        //const filePath = path.join(eventsPath, file);
+        const event = await import(file);
+        console.log("[","\x1b[42m","E","\x1b[0m","]","\x1b[4m", path.basename(file), "\x1b[0m" + " Loaded!")
         if (event.once) {
             client.once(event.name, (...args: any) => event.execute(...args));
         } else {
