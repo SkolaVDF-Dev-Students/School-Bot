e2300d581333df11e15244c08a633fa5eec66336
DEPSTRCZ <jiri.edelmann@gmail.com>
2022-11-16 10:45:21 +0000

    Button collector

--- src/events/verify/GuildMemberAdd/role-send.ts
+++ src/events/verify/GuildMemberAdd/role-send.ts
@@ -1,12 +1,21 @@
 import { unverified_role_id } from "../../../configs/veify.json"
-import { ActionRowBuilder, ButtonBuilder, Embed, EmbedBuilder } from "discord.js"
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Embed, EmbedBuilder } from "discord.js"
+//Send the message with buttoon to start verify proces when user joins
 module.exports = {
     name: "guildMemberAdd",
     async execute(member:any) {
         await member.roles.add(unverified_role_id)
-        const emebed = new EmbedBuilder()
+        const embed = new EmbedBuilder()
         .setTitle("Ovění")
-        .setDescription("")
-        member.send()
+        .setDescription("Prosím ově se BLA BLA BLA")
+
+        const buttonrow = new ActionRowBuilder()
+        .addComponents(
+            new ButtonBuilder()
+            .setCustomId("start-verify")
+            .setLabel("Začít proces ověření")
+            .setStyle(ButtonStyle.Primary)
+        )
+        await member.send({content: `<@&${member.id}>`,emebeds: [embed], components: [buttonrow]})
     }
 }
--- src/events/verify/interactionCreate/begin-sequence.ts
+++ src/events/verify/interactionCreate/begin-sequence.ts
@@ -2,5 +2,16 @@ module.exports = {
     name: "interactionCreate",
     async execute(interaction:any) {
         //Handler for button in verify channel
+        if(interaction.isButton()) return;
+        
+        const filter = (i:any) => i.customId === 'primary' && i.user.id === '122157285790187530';
+
+        const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
+
+        collector.on('collect', async (i:any) => {
+	        await i.update({ content: 'A button was clicked!', components: [] });
+        });
+
+        collector.on('end', (collected:any) => console.log(`Collected ${collected.size} items`));
     }
 }
