c9a09deba923bdb65df61918e7473def14af142c
DEPSTRCZ <jiri.edelmann@gmail.com>
2023-01-02 21:03:44 +0100

    Base done, needs hadnler for nesting and beuty

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ import AutoDeploy from "./handlers/autodeploy"
 import path from "node:path"
 import EventsHandler from "./handlers/events";
 import SlashCommandsHandler from "./handlers/slashcommands";
+
+let CacheStorePath:string = path.join(__dirname,"./store/cachesystem/temp.json")
+let CommandsPath:string = path.join(__dirname,"./commands")
 //intents - client
 const INTENTS = [
     GatewayIntentBits.DirectMessages,
@@ -23,26 +26,35 @@ const INTENTS = [
 const client = new Client({ intents: INTENTS });
 //Loding
 console.log("\x1b[34m", "╔════════════════════════╗", "\x1b[0m", "\n\x1b[36m", "  Bot Core -", "\x1b[0m", "Loading...", "\n\x1b[34m", "╚════════════════════════╝", "\x1b[0m");
-console.log("[", "\x1b[43m", "Commands AutoDeploy", "\x1b[0m", "]", "\x1b[0m","Checking for new commands...")
 //Auto deploy cmds
-fs.readFile(path.join(__dirname,"./store/cachesystem/temp.json"), (err:any, data:any) => {
-    if (!err || data) {
-        const file = JSON.parse(data.toString());
-        console.log(file)
-        const length = fs.readdirSync(path.join(__dirname,"./commands/")).length;
-        file.TEMP.editDeploy = length;
-        fs.writeFile(path.join(__dirname,"./store/cachesystem/temp.json"), JSON.stringify(file), (err:any) => {
-            if (err) console.log(err);
-        });
+try {
+    const ReadStored = fs.readFileSync(CacheStorePath,"utf-8")
+    let DataStored:any = JSON.parse(ReadStored.toString())
+    const DirsLength = fs.readdirSync(CommandsPath,"utf-8").length
+    console.log("[", "\x1b[43m", "Commands AutoDeploy", "\x1b[0m", "]", "\x1b[0m","Checking for new commands...")
+    if(DirsLength !== DataStored.TEMP.editDeploy) {
+        AutoDeploy(client)
+        DataStored.TEMP.editDeploy = DirsLength
+        fs.writeFileSync(CacheStorePath,JSON.stringify(DataStored),"utf-8")
+    } else {
+        console.log("[", "\x1b[43m", "Commands", "\x1b[0m", "]", "\x1b[0m", " No new commands were detected... Skipping");
+
     }
-});
+    console.log("R&W Succesful")
+
+
+} catch(err) {
+    console.log("Dement cement xD")
+    console.log(err)
+}
+/*
 fs.readdir(path.join(__dirname, "./commands"), (err:any, data:any) =>{
     if (TEMP.TEMP.editDeploy !== data.length) {
         AutoDeploy(client)
     } else {
         console.log("[", "\x1b[43m", "Commands", "\x1b[0m", "]", "\x1b[0m", " No new commands were detected... Skipping");
     }
-})
+})*/
 // main handlers
 EventsHandler(client);
 SlashCommandsHandler(client);
